2b06c820268ca1f4f21525a8b2f1af722b4df8a2
Alwaleed Alssamani <aalssamani@gmail.com>
2017-09-17 14:11:39 +1000

    Revert "Revert "starting with adding business section""
    This reverts commit aec0e5706649975fce4f4d845d253a7330683b78.

---
+++ BKP/upload/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/admin/');
+define('HTTP_CATALOG', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/admin/');
+define('HTTPS_CATALOG', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/alwaleed/project_github/opencart/upload/admin/');
+define('DIR_SYSTEM', '/Users/alwaleed/project_github/opencart/upload/system/');
+define('DIR_IMAGE', '/Users/alwaleed/project_github/opencart/upload/image/');
+define('DIR_STORAGE', '/Users/alwaleed/project_github/opencart/storage/');
+define('DIR_CATALOG', '/Users/alwaleed/project_github/opencart/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3307');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
